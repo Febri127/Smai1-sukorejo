d967bb1a117753aef10991b8bf705419d1243132
Febri127 <97936967+Febri127@users.noreply.github.com>
2022-07-27 14:45:43 +0700

    Update login.php
    u

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Login SMAI 1</title>
+	<title>SMAI</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
 	<script src="https://kit.fontawesome.com/a81368914c.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<img class="wave" src="images/user/wave.png">
+	<img class="wafe" src="images/user/wafe.png">
 	<div class="container">
 		<div class="img">
 			<img src="images/user/bg.svg">
