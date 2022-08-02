cc9f02933bcb359fbda2a101b40450d2bd586154
MUIS <muis4346@gmail.com>
2022-08-02 11:44:00 +0700

    Update list_bina.php

--- file/bina/views/list_bina.php
+++ file/bina/views/list_bina.php
@@ -1,6 +1,6 @@
 <div class="card">
     <div class="card-header">
-        <h4>Pembinaan Siswa</h4>
+        <h4><i class="fa-solid fa-user">Pembinaan Siswa</h4>
     </div>
     <div class="card-body">
         <?php
