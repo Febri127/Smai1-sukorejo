cfdeff2d07cd10d16b36886bcfa7738845bad0e6
Febri127 <97936967+Febri127@users.noreply.github.com>
2022-08-02 14:47:07 +0700

    edit

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
-    <title>SMA Ibrahimy 1</title>
+    <title>BKSMAI</title>
 </head>
 
 <body>
