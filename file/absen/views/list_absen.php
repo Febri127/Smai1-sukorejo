8821ad7bc8d36e6261808111c8cfbfd7da6b6b6f
MUIS <muis4346@gmail.com>
2022-08-02 11:43:54 +0700

    Update list_absen.php

--- file/absen/views/list_absen.php
+++ file/absen/views/list_absen.php
@@ -1,7 +1,7 @@
 
 <div class="card">
     <div class="card-header">
-        <h4><i class="fa-solid fa-database"></i>Rekap Absen Siswa</h4>
+        <h4><i class="fa-solid fa-calendar"></i>Rekap Absen Siswa</h4>
     </div>
     <div class="card-body">
         <?php
